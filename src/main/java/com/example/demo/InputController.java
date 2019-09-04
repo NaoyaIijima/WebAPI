6ca4439f2ee9475a779de72a674d218cc382ba3c
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-04 05:16:36 +0000

    コントローラー作った．

---
+++ src/main/java/com/example/demo/InputController.java
@@ -0,0 +1,18 @@
+nputpackage com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Controller
+public class InputController{
+    
+    @GetMapping("/input")
+    public String getInput(){
+        return "input";
+    }
+    
+}
